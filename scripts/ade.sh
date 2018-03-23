eacfafd052f79ce1966ca7b8f5d7dd8ce49211e1
Oliver Batchelor <saulzar@gmail.com>
2018-03-23 22:51:09 +1300

    Remove accidentally added

--- scripts/ade.sh
+++
@@ -1,5 +0,0 @@
-#~/bin/bash
-
-MODEL="pretrained --base 16 --inc 8 --block_layers 1"
-python -m main --name ade_borders  --input /storage/workspace/ade_borders --lr 0.1 --max_scale=1.25 \
-  --min_scale=0.8 --rotation=5 --loss jacc --batch_size 16 --model "$MODEL"  --epochs 50 --image_size 320  $@
